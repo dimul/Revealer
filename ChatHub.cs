7ffd873b59726167d0f7d6d9dbb1df9c06b1f51b
Motiejus <motiejus.b@gmail.com>
2013-04-23 16:36:07 +0200

    Remote ppt test working

--- ChatHub.cs
+++ ChatHub.cs
@@ -6,10 +6,11 @@ namespace Revealer
 {
     public class ChatHub : Hub
     {
-        public void Send(string name, string message)
+        public void Send(string message)
         {
             // Call the broadcastMessage method to update clients.
-            Clients.All.broadcastMessage(name, message);
+            //Clients.All.broadcastMessage(message);
+            Clients.Others.broadcastMessage(message);
         }
     }
 }
